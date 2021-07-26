6f9598758c83feea6eec5f83e18d781555a5200f
Ayush Kapoor <ayushk681@gmail.com>
2021-07-26 19:41:43 +0530

    fourth commit

--- Webnote/settings.py
+++ Webnote/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'r)f!t=!=bc087)io9f)pd6nr0_mbl68%rkirfanfpmitey2p7*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
